c185e60c510531d01e6f5ee35605d7aa2d294ecf
Alexandre Roux <alex@tekartik.com>
2024-04-21 20:15:43 +0200

    fix: ci

--- platform_node/test/context_node_test.dart
+++ platform_node/test/context_node_test.dart
@@ -10,9 +10,22 @@ void main() {
     test('info', () {
       expect(platformContextNode.node, isNotNull);
       var map = platformContextNode.toMap();
-      expect(map, {
-        'node': {'platform': 'linux'}
-      });
+      if (platformContextNode.node!.isLinux) {
+        expect(platformContextNode.platform!.isLinux, isTrue);
+        expect(map, {
+          'node': {'platform': 'linux'}
+        });
+      } else if (platformContextNode.node!.isWindows) {
+        expect(platformContextNode.platform!.isWindows, isTrue);
+        expect(map, {
+          'node': {'platform': 'win32'}
+        });
+      } else if (platformContextNode.node!.isMacOS) {
+        expect(platformContextNode.platform!.isMacOS, isTrue);
+        expect(map, {
+          'node': {'platform': 'darwin'}
+        });
+      }
     });
     test('nodeVersion', () {
       expect(nodeVersion, greaterThan(Version(8, 0, 0)));
