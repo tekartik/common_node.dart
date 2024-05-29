ec243a33e04e21d5b322abca41dcf75db5dab68d
Alexandre Roux <alex@tekartik.com>
2024-05-29 18:50:23 +0200

    wip: starting new interop

--- repo_support/tool/run_ci.dart
+++ repo_support/tool/run_ci.dart
@@ -1,15 +1,18 @@
+import 'dart:io';
+
 import 'package:path/path.dart';
 import 'package:tekartik_build_node/package.dart';
 
+bool get runningOnGithub => Platform.environment['GITHUB_ACTIONS'] == 'true';
 var topDir = '..';
 
 Future<void> main() async {
   for (var dir in [
+    'core_node',
     'platform_node',
     'platform_node_test',
     'fs_node',
     'fs_node_test',
-    'http_node',
     'http_node_test',
   ]) {
     var path = join(topDir, dir);
@@ -21,6 +24,16 @@ Future<void> main() async {
     'http_redirect_node',
   ]) {
     var path = join(topDir, dir);
+
     await nodePackageRunCi(path, NodePackageRunCiOptions(noNodeTest: true));
+  }
+
+  for (var dir in [
+    'http_node',
+  ]) {
+    var path = join(topDir, dir);
+
+    await nodePackageRunCi(
+        path, NodePackageRunCiOptions(noNodeTest: true)); //runningOnGithub));
   }
 }
