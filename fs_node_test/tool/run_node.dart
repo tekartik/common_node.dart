a1b158cfd7a93fe8f6c4782164574a130f0d5c74
Alexandre Roux <alex@tekartik.com>
2024-08-10 11:39:18 +0200

    windows test

--- fs_node_test/tool/run_node.dart
+++ fs_node_test/tool/run_node.dart
@@ -1,8 +1,5 @@
-import 'package:process_run/shell.dart';
+import 'package:tekartik_build_node/build_node.dart';
 
 Future main() async {
-  var shell = Shell();
-  await shell.run('''
-node build/node/main.dart.js
-  ''');
+  await nodePackageRun('.');
 }
--- platform_node_test/tool/build_and_run.dart
+++ platform_node_test/tool/build_and_run.dart
@@ -1,11 +1,6 @@
-import 'package:process_run/shell.dart';
 import 'package:tekartik_build_node/build_node.dart';
 
 Future main() async {
-  await nodeBuild();
-  var shell = Shell();
-
-  await shell.run('''
-  node build/node/main.dart.js argtmp
-  ''');
+  await nodePackageCompileJs('.', input: 'node/main.dart');
+  await nodePackageRun('.');
 }
